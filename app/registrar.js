5de2416a0ac99a273d1328bcd11ca4326f42de47
orbis02 <44383676+orbis02@users.noreply.github.com>
2018-11-17 00:41:53 -0400

    blog api
    blog api

---
+++ app/registrar.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+$("#registrar").click(function()
+{ 
+     registrar();
+  
+});
+});
+
+
+function registrar()
+{  var name=$("#name").val();
+  var email=$("#email").val();
+  var password=$("#password").val();
+  var password2=$("#password2").val();
+
+  if(password!=password2)
+    {
+        alert("las contrase;as son diferentes");
+        return;
+    }
+    var data={
+        name: name,
+        email:email,
+        password:password
+    }
+    fetch("http://68.183.27.173:8080/register",{
+        method:'POST',
+        body:JSON.stringify(data),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }).then(res=>res.json())
+    .then(response=>console.log('Success:',JSON.stringify(response)))
+    .catch(error=>console.log("Error:",error));
+}
